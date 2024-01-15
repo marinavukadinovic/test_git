034dcc0c96adba33e71501ae8a3ea045f9478def
marinavukadinovic <marina.vukadinovic@hak-reutte.ac.at>
2024-01-15 10:10:01 +0100

    made a change

--- src/test_github/Test.java
+++ src/test_github/Test.java
@@ -3,8 +3,9 @@ package test_github;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Hello Github!");
+		System.out.println("test 2");
 	}
 
 }
