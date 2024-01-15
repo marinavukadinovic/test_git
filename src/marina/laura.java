17c73a81b177ed5f3c463d35844ba1b476c97103
marinavukadinovic <marina.vukadinovic@hak-reutte.ac.at>
2024-01-15 10:32:03 +0100

    made a change

---
+++ src/marina/laura.java
@@ -0,0 +1,5 @@
+package marina;
+
+public class laura {
+
+}
---
+++ src/marina/test.java
@@ -0,0 +1,13 @@
+package marina;
+
+public class test {
+
+	public static void main(String[] args) {
+
+
+	System.out.println("Hello Github!");
+	System.out.println("test 2");
+	System.out.println("Local commit test end");
+			}
+
+		}
