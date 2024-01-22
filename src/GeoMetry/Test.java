a65439127af3754a8dce72e99be76aeb14d88e22
marinavukadinovic <marina.vukadinovic@hak-reutte.ac.at>
2024-01-22 11:05:15 +0100

    created dcoordinates

---
+++ src/GeoMetry/Test.java
@@ -0,0 +1,13 @@
+package GeoMetry;
+
+public class Test {
+
+
+	public static void main(String[] args) {
+		
+		Coordinate c1 = new Coordinate(1, 7);
+		Coordinate c2 = new Coordinate(3, 5);
+		Coordinate c3 = new Coordinate(9, 2);
+	}
+		
+	}
