90880ec0f602345d0e9017c995bc8d0387b229b2
marinavukadinovic <marina.vukadinovic@hak-reutte.ac.at>
2024-01-22 10:13:47 +0100

    created constructor and getter and setter

--- src/GeoMetry/Circle.java
+++ src/GeoMetry/Circle.java
@@ -5,4 +5,37 @@ public class Circle extends Shapes{
 	Coordinate middle;
 	Coordinate radiusPoint;
 	double radius;
+	
+	public Circle(Coordinate middle, Coordinate radiusPoint) {
+		super();
+		this.middle = middle;
+		this.radiusPoint = radiusPoint;
+	}
+	
+
+	public Coordinate getMiddle() {
+		return middle;
+	}
+
+	public void setMiddle(Coordinate middle) {
+		this.middle = middle;
+	}
+
+	public Coordinate getRadiusPoint() {
+		return radiusPoint;
+	}
+
+	public void setRadiusPoint(Coordinate radiusPoint) {
+		this.radiusPoint = radiusPoint;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+
 }
