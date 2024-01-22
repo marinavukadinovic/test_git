8b8d45a091c249e33dfa8fe918d3e5e9b960b1ec
marinavukadinovic <marina.vukadinovic@hak-reutte.ac.at>
2024-01-22 10:04:37 +0100

    created getter and setter

--- src/GeoMetry/Coordinate.java
+++ src/GeoMetry/Coordinate.java
@@ -2,4 +2,29 @@ package GeoMetry;
 
 public class Coordinate {
 
+	double x;
+	double y;
+	
+	public Coordinate(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+	
 }
+
