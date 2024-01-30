af6258d955f8e445e4d8eb3896bcdd1a4d99504e
marinavukadinovic <marina.vukadinovic@hak-reutte.ac.at>
2024-01-30 09:03:42 +0100

    added method to claculate total area, calculate total perimeter and one to get the largest shape

--- src/GeoMetry/Group.java
+++ src/GeoMetry/Group.java
@@ -1,8 +1,101 @@
-package GeoMetry;
+package geoMetry;
+ 
+import java.util.ArrayList;
 
-public class Group {
-	public double addArea() {
+import java.util.HashSet;
 
-		return 0;
+import java.util.Set;
+ 
+ 
+	 public class Group {
+
+	    private ArrayList<Shape> shape = new ArrayList<>();
+ 
+	    public void addShape(Shape shape) {
+
+	        shape.add(shape);
+
+		    public void removeShape(Shape shape) {
+
+		        shape.remove(shape);
+
+		    }
+	 
+		    public double totalArea() {
+
+		        double totalArea = 0;
+
+		        Set<Shape> seenShapes = new HashSet<>();
+
+				return totalArea;
+	 
+		
+
+		    }
+
+
+		public double calculateTotalArea() {
+
+	        double totalArea = 0;
+
+	        Set<Shape> seenShape = new HashSet<>();
+
+	        for (Shape shape : seenShape) {
+
+	            if (!seenShape.contains(shape)) {
+
+	                totalArea += shape.calculateArea();
+
+	                seenShape.add(shape);
+
+	            }
+
+	        }
+
+	        return totalArea;
+
+	    }
+
+		public double calculateTotalPerimeter() {
+
+	        double totalPerimeter = 0;
+
+	        Set<Shape> seenShape = new HashSet<>();
+	 
+	        for (Shape shape : shape) {
+
+	            if (!seenShape.contains(shape)) {
+
+	                totalPerimeter += shape.calculateArea();
+
+	                seenShape.add(shape);
+
+	            }
+
+	        }
+	 
+	        return totalPerimeter;
+
+	    }
+
+		public Shape largestShape(){
+
+			double maxArea = 0.0;
+
+			for (Shape shape : shapes) {
+
+	            double area = shape.calculateArea();
+
+	            if( area >= maxArea ) {
+
+	            	maxArea = area;
+
+	            }
+
+	        } 
+
 	}
-}
+	 
+	 
+	    }
+ 
