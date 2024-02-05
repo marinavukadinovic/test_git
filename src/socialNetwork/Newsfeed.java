c8e316bb963e19827bf768f0798d9634bf183c91
marinavukadinovic <marina.vukadinovic@hak-reutte.ac.at>
2024-02-05 11:04:05 +0100

    attributes + created constructor + created getters and setters

--- src/socialNetwork/Newsfeed.java
+++ src/socialNetwork/Newsfeed.java
@@ -1,5 +1,39 @@
 package socialNetwork;
 
-public class Newsfeed {
+import java.util.ArrayList;
 
+public class Newsfeed{
+	 
+	private ArrayList<Newsfeed>newsfeed;
+	private String title;
+	private String content;
+	
+	
+	
+	public Newsfeed(ArrayList<Newsfeed> newsfeed, String title, String content) {
+		super();
+		this.newsfeed = newsfeed;
+		this.title = title;
+		this.content = content;
+	}
+	public ArrayList<Newsfeed> getNewsfeed() {
+		return newsfeed;
+	}
+	public void setNewsfeed(ArrayList<Newsfeed> newsfeed) {
+		this.newsfeed = newsfeed;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
 }
