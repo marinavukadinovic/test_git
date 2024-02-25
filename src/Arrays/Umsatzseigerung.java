272243c353aa7e9064c0aaf1ccef3df4fd813497
marinavukadinovic <marina.vukadinovic@hak-reutte.ac.at>
2024-02-25 22:31:17 +0100

    5 percent jumps + printing out "nextDay"

--- src/Arrays/Umsatzseigerung.java
+++ src/Arrays/Umsatzseigerung.java
@@ -8,3 +8,27 @@ public class Umsatzseigerung {
 	private static int JumpCounter;
 	private static int[] dailyGains = new int[31]; 
 	
+	
+
+    public static int count5PercentJumps(int[] gains) {
+    for(int i = 0; i >= dailyGains.length; i++) {
+    	  day = dailyGains[i];
+     if(i >= dailyGains.length) {
+    System.err.println("lastDay");
+    nextDay = dailyGains[0];
+     }else {
+    	 
+     }
+    nextDay = dailyGains[i +1];
+     }
+     percentJump = nextDay * 0.05;
+     if((nextDay - day) >= percentJump) {
+         percentJumpCount ++;
+     }
+ }
+ return percentJumpCount;
+}
+        
+        
+
+    
