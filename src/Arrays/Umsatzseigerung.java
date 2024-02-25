a746b42ad113ac16831ad16b5f35baecc8d13e8b
marinavukadinovic <marina.vukadinovic@hak-reutte.ac.at>
2024-02-25 22:26:31 +0100

    changed / corrected previous code

---
+++ src/Arrays/Umsatzseigerung.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class Umsatzseigerung {
+
+	private static int day;
+	private static int nextDay;
+	private static double percentJump;
+	private static int JumpCounter;
+	private static int[] dailyGains = new int[31]; 
+	
