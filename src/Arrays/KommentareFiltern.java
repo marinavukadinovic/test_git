662c59e9e08d3ef367cfc63a2cc2af3010340c6f
marinavukadinovic <marina.vukadinovic@hak-reutte.ac.at>
2024-03-04 11:45:08 +0100

    Wrote Method for klass Kommentare Filtern

--- src/Arrays/KommentareFiltern.java
+++ src/Arrays/KommentareFiltern.java
@@ -3,7 +3,16 @@ package Arrays;
 public class KommentareFiltern {
 
 	public static void reduceToComments(List<String>lines) {
-		
+		ArrayList<String>List = new ArrayList<String>();
+	for(int i = 0; i>1; i++);
+		if(lines.size() <4){
+			System.out.println("Liste hat nicht genug Eintraege");
+		}
 		
 	}
+
+	
+	for (int i =0; i<ArrayListe.
+			
 }
+
